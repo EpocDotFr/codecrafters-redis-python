eb58efbe0d8684e3fef0e18b90b15f8e6a94a22a
EpocDotFr <contact@epoc.fr>
2023-04-19 16:52:40 +0200

    Implement full protocol

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
+from typing import Union, List, Optional
 import socketserver
+
+
+class SimpleStringType(str):
+    pass
+
+
+class BulkStringType(str):
+    pass
+
+
+class ErrorType(str):
+    pass
 
 
 class RedisServer(socketserver.ThreadingTCPServer):
@@ -7,8 +20,92 @@ class RedisServer(socketserver.ThreadingTCPServer):
 
 
 class RESPHandler(socketserver.StreamRequestHandler):
-    def handle(self):
-        self.wfile.write('+PONG\r\n'.encode())
+    def receive(self, length: Optional[int] = None) -> str:
+        if length:
+            frame = self.rfile.read(length + 2).decode() # Also read \r\n termination chars
+        else:
+            frame = self.rfile.readline().decode()
+
+        if not frame:
+            return ''
+
+        return frame[:-2] # Remove \r\n termination chars
+
+    def send(self, data: Optional[Union[List, BulkStringType, SimpleStringType, ErrorType, int]]) -> None:
+        self.wfile.write(self.pack(data).encode())
+
+    def handle(self) -> None:
+        while True:
+            frame = self.receive()
+
+            if not frame:
+                break
+
+            request = self.unpack(frame)
+
+            if not isinstance(request, list):
+                raise ValueError('Expected array from client')
+
+            command, args = request[0], request[1:]
+
+            if command == 'PING':
+                if not args:
+                    self.send(SimpleStringType('PONG'))
+                else:
+                    self.send(BulkStringType(args[0]))
+            else:
+                raise ValueError('Unknown command')
+
+    def unpack(self, frame: str) -> Optional[Union[List, BulkStringType, SimpleStringType, ErrorType, int]]:
+        type, body = frame[0], frame[1:]
+
+        if type == '*': # Array
+            length = int(body)
+
+            return [
+                self.unpack(self.receive()) for _ in range(length)
+            ]
+        elif type == '$': # Bulk string
+            length = int(body)
+
+            if length == -1: # Null bulk string
+                return None
+
+            return BulkStringType(self.receive(length))
+        elif type == ':': # Integer
+            return int(body)
+        elif type == '-': # Error
+            return ErrorType(body)
+        elif type == '+': # Simple string
+            return SimpleStringType(body)
+
+        raise ValueError('Unknown type')
+
+    def pack(self, data: Optional[Union[List, BulkStringType, SimpleStringType, ErrorType, int]]) -> str:
+        frame = ''
+
+        if data is None: # Null bulk string
+            frame = '$-1'
+        elif isinstance(data, BulkStringType): # Bulk string
+            frame = f'${len(data)}\r\n{data}'
+        elif isinstance(data, SimpleStringType): # Simple string
+            frame = f'+{data}'
+        elif isinstance(data, ErrorType): # Error
+            frame = f'-{data}'
+        elif isinstance(data, int): # Integer
+            frame = f':{data}'
+        elif isinstance(data, list): # Array
+            frame = '*{}\r\n{}'.format(
+                len(data),
+                ''.join([
+                    self.pack(item) for item in data
+                ])
+            )
+
+        if not frame:
+            raise ValueError('Unhandled type')
+
+        return frame + '\r\n'
 
 
 def main():
@@ -18,5 +115,5 @@ def main():
         except KeyboardInterrupt:
             pass
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
