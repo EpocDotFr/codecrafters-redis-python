32d35c81b7fdc8347c772d87b4ed1935792f448f
EpocDotFr <contact@epoc.fr>
2023-04-20 14:01:48 +0200

    Finished

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from typing import Union, List, Optional
+from typing import Union, List, Optional, Dict
+from argparse import Namespace
+from time import time
 import socketserver
 
 
@@ -17,14 +19,17 @@ class ErrorType(str):
 class RedisServer(socketserver.ThreadingTCPServer):
     allow_reuse_address = True
     daemon_threads = True
+    store: Dict
 
     def __init__(self, *args, **kvargs):
-        super(RedisServer, self).__init__(*args, **kvargs)
+        super().__init__(*args, **kvargs)
 
         self.store = {}
 
 
 class RESPHandler(socketserver.StreamRequestHandler):
+    server: RedisServer
+
     def receive(self, length: Optional[int] = None) -> str:
         if length:
             frame = self.rfile.read(length + 2).decode() # Also read \r\n termination chars
@@ -36,8 +41,35 @@ class RESPHandler(socketserver.StreamRequestHandler):
 
         return frame[:-2] # Remove \r\n termination chars
 
-    def send(self, data: Optional[Union[List, BulkStringType, SimpleStringType, ErrorType, int]]) -> None:
-        self.wfile.write(self.pack(data).encode())
+    def send(self, data: Optional[Union[List, BulkStringType, SimpleStringType, ErrorType, int]]) -> int:
+        return self.wfile.write(self.pack(data).encode())
+
+    def parse_args(self, request: List[BulkStringType], args: Optional[List[str]] = None, kvargs: Optional[List[str]] = None) -> Namespace:
+        all_args = request[1:]
+        args = args if args is not None else []
+        kvargs = kvargs if kvargs is not None else []
+
+        ret = Namespace()
+
+        for i, arg in enumerate(args):
+            try:
+                value = all_args[i]
+            except IndexError:
+                value = None
+
+            setattr(ret, arg, value)
+
+        for kvarg in kvargs:
+            try:
+                kvarg_index = all_args.index(BulkStringType(kvarg)) # Locate the arg name
+
+                value = all_args[kvarg_index + 1] # Arg value is found just after the named arg
+            except (ValueError, IndexError):
+                value = None
+
+            setattr(ret, kvarg, value)
+
+        return ret
 
     def handle(self) -> None:
         while True:
@@ -48,30 +80,41 @@ class RESPHandler(socketserver.StreamRequestHandler):
 
             request = self.unpack(frame)
 
-            if not isinstance(request, list):
-                raise ValueError('Expected array from client')
+            if not request or not isinstance(request, list):
+                raise ValueError('Expected non-empty array from client')
 
-            command, args = request[0].lower(), request[1:]
+            for part in request:
+                if not isinstance(part, BulkStringType):
+                    raise ValueError('Expected non-empty array of bulk strings from client')
 
-            if command == 'ping':
-                if not args:
-                    self.send(SimpleStringType('PONG'))
-                else:
-                    self.send(BulkStringType(args[0]))
-            elif command == 'echo':
-                self.send(BulkStringType(args[0]))
-            elif command == 'set':
-                key, value = args[0], args[1]
+            command = request[0]
 
-                self.server.store[key] = value
+            if command == 'COMMAND':
+                break # Ignore COMMAND connection
+            elif command == 'PING':
+                self.send(SimpleStringType('PONG'))
+            elif command == 'ECHO':
+                args = self.parse_args(request, args=['message'])
+
+                self.send(args.message)
+            elif command == 'SET':
+                args = self.parse_args(request, args=['key', 'value'], kvargs=['PX'])
+
+                self.server.store[args.key] = (
+                    args.value,
+                    (int(time()) + int(int(args.PX) / 1000)) if args.PX else None
+                )
 
                 self.send(SimpleStringType('OK'))
-            elif command == 'get':
-                key = args[0]
+            elif command == 'GET':
+                args = self.parse_args(request, args=['key'])
 
-                value = self.server.store.get(key)
+                value, ttl = self.server.store.get(args.key, (None, None))
 
-                self.send(None if not value else BulkStringType(value))
+                if isinstance(ttl, int) and int(time()) >= ttl:
+                    value = None
+
+                self.send(BulkStringType(value) if value else None)
             else:
                 raise ValueError('Unknown command')
 
@@ -127,7 +170,7 @@ class RESPHandler(socketserver.StreamRequestHandler):
         return frame + '\r\n'
 
 
-def main():
+def main() -> None:
     with RedisServer(('127.0.0.1', 6379), RESPHandler) as server:
         try:
             server.serve_forever()
